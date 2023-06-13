9fef25fc444952593cbdabe1de44e75517fb720c
xkolsha <aviad85@outlook.com>
2023-06-13 20:56:51 -0300

    removed old func

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,1 @@
 const API_KEY = "{c8f235379537ae30e3dbf9ffbf148ad8}";
-
-document
-  .getElementById("search-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    const city = document.getElementById("city-input").value;
-    getGeocodingData(city);
-  });
